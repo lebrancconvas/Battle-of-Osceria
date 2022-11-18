69deb605a2edef2c661ae6eb2182f5948e757b79
lebrancconvas <lebrancconvas@gmail.com>
2022-11-18 22:14:34 +0700

    🎉 feat(init): init project

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/lebrancconvas/Battle-of-Osceria/backend
+
+go 1.19
---
+++ backend/main.go
@@ -0,0 +1,11 @@
+package main
+
+type Mage struct {
+	name string;
+	level int;
+	fire int; 
+}
+
+func main() {
+	
+}
